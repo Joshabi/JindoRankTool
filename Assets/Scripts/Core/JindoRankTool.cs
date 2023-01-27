ee7ac3613b5c1d7795d7c033af361d36e261f841
Oli S-L <oshannonlepper@users.noreply.github.com>
2023-01-27 01:15:52 +0000

    Update the CSV input to support specific difficulties per map

--- Assets/Scripts/Core/JindoRankTool.cs
+++ Assets/Scripts/Core/JindoRankTool.cs
@@ -15,6 +15,8 @@ public class JindoRankTool : MonoBehaviour
 
     [SerializeField] private float _timeScale = 1.0f;
 
+    private Dictionary<string, HashSet<BeatmapDifficultyRank>> _mapDifficulties;
+
     private LevelPreview _levelPreview;
     private LevelLoader _levelLoader;
     private LevelDownloader _levelDownloader;
@@ -40,6 +42,25 @@ public class JindoRankTool : MonoBehaviour
 
         if (_beatSaberMapIDCSV.Length > 0)
         {
+            _mapDifficulties = new Dictionary<string, HashSet<BeatmapDifficultyRank>>();
+            string[] codes = _beatSaberMapIDCSV.Split(',');
+            foreach (string code in codes)
+            {
+                if (code.Contains(':'))
+                {
+                    string levelID = code.Split(':')[0];
+                    BeatmapDifficultyRank difficulty = StringToDifficulty(code.Split(':')[1]);
+                    if (_mapDifficulties.ContainsKey(levelID))
+                    {
+                        _mapDifficulties[levelID].Add(difficulty);
+                    }
+                    else
+                    {
+                        _mapDifficulties.Add(levelID, new HashSet<BeatmapDifficultyRank>() { difficulty });
+                    }
+                }
+            }
+
             _levelDownloader = GetComponent<LevelDownloader>();
             _levelDownloader.OnLevelDownloadsCompleted += OnLevelDownloadsComplete;
             _levelDownloader.DownloadLevels(_beatSaberMapIDCSV);
@@ -56,7 +77,18 @@ public class JindoRankTool : MonoBehaviour
         string[] levelPaths = System.IO.Directory.GetDirectories(PathUtils.GetImportDirectory());
         foreach (string levelPath in levelPaths)
         {
-            _levelLoader.LoadLevel(levelPath, _levelLoader_OnLevelLoaded);
+            string code = levelPath.Split('\\')[^1];
+            if (_mapDifficulties.ContainsKey(code))
+            {
+                foreach (BeatmapDifficultyRank difficulty in _mapDifficulties[code])
+                {
+                    _levelLoader.LoadLevel(levelPath, difficulty, _levelLoader_OnLevelLoaded);
+                }
+            }
+            else
+            {
+                _levelLoader.LoadLevel(levelPath, _levelLoader_OnLevelLoaded);
+            }
         }
     }
 
@@ -69,5 +101,19 @@ public class JindoRankTool : MonoBehaviour
     private void _levelLoader_OnLevelLoaded(BeatmapData beatmapData)
     {
         _sliceMapOutputter.ProcessBeatmap(beatmapData);
+    }
+    private BeatmapDifficultyRank StringToDifficulty(string inDifficulty)
+    {
+        switch (inDifficulty.ToLower())
+        {
+            case "easy": return BeatmapDifficultyRank.Easy;
+            case "normal": return BeatmapDifficultyRank.Normal;
+            case "hard": return BeatmapDifficultyRank.Hard;
+            case "expert": return BeatmapDifficultyRank.Expert;
+            case "expertplus": return BeatmapDifficultyRank.ExpertPlus;
+            case "expert+": return BeatmapDifficultyRank.ExpertPlus;
+        }
+
+        return BeatmapDifficultyRank.Easy;
     }
 }
